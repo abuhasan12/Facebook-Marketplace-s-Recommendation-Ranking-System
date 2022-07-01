66d9dcb34d9ec7e542c015687a15636d3020359a
unknown <abu_h@live.co.uk>
2022-07-01 17:47:40 +0100

    image torch dataset

--- image_torch_dataset.py
+++ image_torch_dataset.py
@@ -1,10 +1,8 @@
-from tokenize import Double
 import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-import seaborn as sns
 
 class ProductsImDataset(Dataset):
     def __init__(self, transform = None):
@@ -14,44 +12,7 @@ class ProductsImDataset(Dataset):
     
     def __getitem__(self, index):
         item = self.data.iloc[index]
-        features = torch.moveaxis(torch.tensor(item[0]), 2, 0)
-        features = features/255
-        if self.transform:
-            features = self.transform(features)
-        label = torch.tensor(int(item[1]))
-        return (features, label)
-    
-    def __len__(self):
-        return len(self.data)
-
-class ProductsImTrainDataset(Dataset):
-    def __init__(self, transform = None):
-        super().__init__()
-        self.data = pd.read_pickle('product_training_df.pkl')
-        self.transform = transform
-    
-    def __getitem__(self, index):
-        item = self.data.iloc[index]
-        features = torch.moveaxis(torch.tensor(item[0]), 2, 0)
-        features = features/255
-        if self.transform:
-            features = self.transform(features)
-        label = torch.tensor(int(item[1]))
-        return (features, label)
-    
-    def __len__(self):
-        return len(self.data)
-
-class ProductsImTestDataset(Dataset):
-    def __init__(self, transform = None):
-        super().__init__()
-        self.data = pd.read_pickle('product_testing_df.pkl')
-        self.transform = transform
-    
-    def __getitem__(self, index):
-        item = self.data.iloc[index]
-        features = torch.moveaxis(torch.tensor(item[0]), 2, 0)
-        features = features/255
+        features = torch.tensor(np.moveaxis(item[0], 2, 0))/255
         if self.transform:
             features = self.transform(features)
         label = torch.tensor(int(item[1]))
@@ -61,14 +22,7 @@ class ProductsImTestDataset(Dataset):
         return len(self.data)
     
 if __name__ == '__main__':
-    # transform = transforms.Compose([
-    #     transforms.Grayscale(),
-    #     transforms.ToTensor()
-    # ])
-
-    transform = transforms.Grayscale()
-
-    im_dataset = ProductsImDataset(transform=transform)
+    im_dataset = ProductsImTestDataset()
     print(len(im_dataset))
     print(im_dataset[12])
 
